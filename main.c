132f80002bef31c0db43db71eb2cb8335d0e4e58
Xianglong He <admin@hxlxz.com>
2015-09-10 22:27:42 +0800

    增加版权声明

--- main.c
+++ main.c
@@ -1,3 +1,24 @@
+/*//////////////////////////GPL开源许可证////////////////////////////////////////////////
+    Copyright (C) <2014>  <Xianglong He>
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	文件名：main.c
+	作者：何相龙
+	邮箱：qwgg9654@gmail.com
+		  admin@hxlxz.com
+	功能描述：串口遥控空调主程序
+*////////////////////////////////////////////////////////////////////////////////////////
 #include<stc15.h>
 #include<UART.h>
 void IR_Send(char *dat, int len);
